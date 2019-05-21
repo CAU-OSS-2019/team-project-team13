e41179f769ec4c9ac314833107976243276a16a9
choiyoungjae <dudwo1344@gmail.com>
2019-05-21 21:25:46 +0900

    show content Dom Struct

--- html_extract/script.js
+++ html_extract/script.js
@@ -1,32 +1,37 @@
 $(function(){
-
-    function childrenSearch(parent,blank){
+    var count = 0;
+    function childrenSearch(parent,parentId){
+        console.log(parent.tagName+"count:"+count);
         //CODE인 부분은 제외하였다. 만약 children이 없는 경우(text이거나, leaf 노드일경우) 이 함수를 실행하지 않는다.
-        if(parent.children && parent.tagName !="CODE"){
-            var parentName = "";
-            // children 노드를 생성하고
+        if(parent.tagName !="CODE"){
+            var beforeParentId = parentId;
+            var parentILtag = '<li';
+            var chilrenOLtag = '<ol';
             var children = parent.children;
-            // parent랑 children이랑 구별하기 위해서 blank의 개수를 늘려서 구분한다.
-            blank = blank + 1;
-            for(var j=0; j<blank ; j++){
-                //해당 블랭크만큼 *을 추가한다.(e.g 첫 div 인경우 *div로 나온다.)
-                parentName += "*";
-            }
-            parentName += parent.tagName;
-            //해당 tag 가 id값이 있는경우 id를 추가해준다.
+            var parentTagName = parent.tagName.toLowerCase();
             if(parent.hasAttribute("id")){
-                parentName += ", id="+parent.getAttribute("id");
+                parentId = parent.tagName.toLowerCase()+ "__"+parent.getAttribute("id");
+            }else{
+                if(parent.hasAttribute("class")){
+                    parentId += "**" +parent.tagName.toLowerCase() + "__" + parent.getAttribute("class");
+
+                }else{
+                    parentId += "**" +parent.tagName.toLowerCase()
+                }
             }
-            //해당 tag 가 class 값이 있는경우 class를 추가해준다.
-            if(parent.hasAttribute("class")){
-                parentName += ", class="+parent.getAttribute("class");
-            }
-            //예로 ***DIV id=test,class=testing
-            //해당 parent에 저장된 tag,id,class값을 console창에 출력한다.
-            console.log(parentName);
+            parentILtag += ' id="'+parentId;
+            chilrenOLtag += ' id="'+parentId;
+            parentILtag += '">';
+            chilrenOLtag += '">';
+            parentILtag += parent.tagName +'</li>';
+            chilrenOLtag += '</ol>';
+            var createQueryId = beforeParentId;
+            count = count +1;
+            $("ol#"+createQueryId).append(parentILtag);
+            $("ol#"+createQueryId + " li#"+parentId).append(chilrenOLtag);
             for(var i=0; i< children.length ; i++){
                 //children의 갯수만큼 재귀 함수로 각 children을 parent처럼 돌린다.
-                childrenSearch(children[i],blank);
+                childrenSearch(children[i],parentId);
             }
         }
       }
@@ -42,9 +47,12 @@ $(function(){
             console.log(html);
             //각 html 태그의 하위노드에 뭐가 있는지 확인하고, 해당 tree구조를 보여준다.
             for(var i = 0;i<html.length;i++){
-               
-               childrenSearch(html[i],0);
-            }  
+                if(html[i].children){
+                    //console.log(i);
+                    childrenSearch(html[i],"ol__contentBody");   
+                }
+            }
+            console.log(count);  
           });
     });
 });
