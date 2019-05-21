acacd0c543252cca2a2f756610709478cfcf386e
choiyoungjae <dudwo1344@gmail.com>
2019-05-21 23:34:42 +0900

    add hover event and complete prototype

--- html_extract/script.js
+++ html_extract/script.js
@@ -13,7 +13,7 @@ $(function(){
                 parentId = parent.tagName.toLowerCase()+ "__"+parent.getAttribute("id");
             }else{
                 if(parent.hasAttribute("class")){
-                    parentId += "**" +parent.tagName.toLowerCase() + "__" + parent.getAttribute("class");
+                    parentId += "**" +parent.tagName.toLowerCase() + "_*_" + parent.getAttribute("class");
 
                 }else{
                     parentId += "**" +parent.tagName.toLowerCase()
@@ -25,7 +25,18 @@ $(function(){
             chilrenOLtag += '">';
             parentILtag += parent.tagName +'</li>';
             chilrenOLtag += '</ol>';
+            // console.log(parentILtag);
+            // console.log(chilrenOLtag);
+            
+            // beforeParentId &&로 split한다음에 각각의 속성들을 조합하여, 해당 ol을 찾는다.
+            //var splitId = beforeParentId.split("**");
             var createQueryId = beforeParentId;
+            //for(var i=0; i<splitId.length-1; i++){
+            //    createQueryId += splitId[i] + ">";
+            //}
+            //createQueryId += splitId[i];
+            // console.log(createQueryId);
+            // console.log(parentId);
             count = count +1;
             $("ol#"+createQueryId).append(parentILtag);
             $("ol#"+createQueryId + " li#"+parentId).append(chilrenOLtag);
@@ -34,6 +45,9 @@ $(function(){
                 childrenSearch(children[i],parentId);
             }
         }
+      }
+      function mouseHover(){
+
       }
       //button을 클릭하면, content page의 html을 글거온다.
     $('#test').click(function(){
@@ -44,15 +58,95 @@ $(function(){
             //해당 html String을 jquery로 hmtl object형식으로 바꿔준다.  
             var domString = result[0];
             var html = $.parseHTML(domString);
-            console.log(html);
             //각 html 태그의 하위노드에 뭐가 있는지 확인하고, 해당 tree구조를 보여준다.
             for(var i = 0;i<html.length;i++){
                 if(html[i].children){
-                    //console.log(i);
                     childrenSearch(html[i],"ol__contentBody");   
                 }
             }
-            console.log(count);  
+            $(document).ready(function(){   
+                /*** Add attribute ***/
+                $("body").click(function(){
+                    var e = window.event;
+                    console.log(e);
+                    if (e.target) {
+                    targ=e.target;
+                } else if (e.srcElement) {
+                    targ=e.srcElement;
+                }
+                var tname;
+                tname = targ.tagName;
+                alert("this target tagName :" + tname + ",Id is" + targ.getAttribute("id"));
+                });
+                //** Add attrubute **/
+                var conbineAtt = "";
+                var colorOfAtt ="";
+                $('li').hover( function() {
+                    $( this ).css( 'color', 'red' );
+                    if(conbineAtt){
+                        conbineAtt = "";
+                    }
+                    //id의 value를 id와 class로 나누고, content page에서 해당 id값을 가진 곳을 가르켜보자
+                    var classify = this.getAttribute("id").split("**");
+                    var idClassify = "__";
+                    var classClassify = "_*_";
+                    console.log(classify);
+                    for(var i =0; i<classify.length-1;i++){
+                        if(classify[i].indexOf(idClassify)>-1){
+                            var temp_id = classify[i].split(idClassify);
+                            conbineAtt += temp_id[0] + "#"+temp_id[1] + " ";
+                        }else{
+                            if(classify[i].indexOf(classClassify)>-1){
+                                var temp_id = classify[i].split(classClassify);
+                                conbineAtt += temp_id[0] + "."+temp_id[1] + " ";
+                            }else{
+                                conbineAtt += classify[i] + " ";
+                            }
+                        }
+                    }
+                    if(classify[i].indexOf(idClassify)>-1){
+                        var temp_id = classify[i].split(idClassify);
+                        conbineAtt += temp_id[0] + "#"+temp_id[1];
+                    }else{
+                        if(classify[i].indexOf(classClassify)>-1){
+                            var temp_id = classify[i].split(classClassify);
+                            conbineAtt += temp_id[0] + "."+temp_id[1];
+                        }else{
+                            conbineAtt += classify[i];
+                        }
+                    }
+                    console.log("conbinAtt : "+conbineAtt);
+                    chrome.tabs.executeScript(null,{
+                        code: 'var conbineAtt = "' + conbineAtt+'";'
+                      },function(){
+                        chrome.tabs.executeScript(null,{
+                            file : 'jquery-3.1.0.min.js'
+                        },function(){
+                            chrome.tabs.executeScript(null,{
+                                file : 'contentEvent.js'
+                            });
+                        });
+                      });
+                  }, function () {
+                    $( this ).css( 'color', 'black' );
+                    console.log("conbinAtt2 : "+conbineAtt);
+                    if(conbineAtt){
+                        console.log("conbinAtt2 : "+conbineAtt);
+                        chrome.tabs.executeScript(null,{
+                            code: 'var conbineAtt = "' + conbineAtt+'";'
+                          },function(){
+                            chrome.tabs.executeScript(null,{
+                                file : 'jquery-3.1.0.min.js'
+                            },function(){
+                                chrome.tabs.executeScript(null,{
+                                    file : 'contentEvent2.js'
+                                });
+                            });
+                          });
+                    }
+                    conbineAtt = "";
+                  });
+            });
           });
     });
 });
